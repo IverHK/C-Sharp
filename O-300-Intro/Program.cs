d0487fe918f3043ec3a8d9730091917caac4fb39
IverHK <iverhk@gmail.com>
2024-11-16 12:04:51 +0100

    test

---
+++ O-300-Intro/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace O;
+
+class Program
+{
+    private static void Main(string[] args)
+    {
+        
+        var nameinput = AskQuestionString("Hello, What is you name? ");
+        Console.WriteLine($"Hello, {nameinput}!");
+        
+        var ageInput = AskQuestionInt("What is your birthyear? ");
+        var hadBirthday = AskQuestionBool("Have you had birthday this year? ");
+        var age = 2024 - Convert.ToInt32(ageInput);
+        if (!hadBirthday) age--;
+        Console.WriteLine($"you are, {age}");
+    }
+
+    public static string AskQuestionString(string question)
+    {
+        Console.Write($"{question}: ");
+        return Console.ReadLine();
+    }    
+    
+    public static string AskQuestionInt(string question)
+    {
+        var birthYear = AskQuestionString(question);
+        return birthYear;
+    }
+    
+    public static bool AskQuestionBool(string question)
+    {
+        var answer = AskQuestionString(question + " (y/n)");
+        return answer.ToLower().StartsWith("j");
+    }
+}
