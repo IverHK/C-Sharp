7cfac69453a19c96c13b7f6528c44cc2005ac4c8
IverHK <iverhk@gmail.com>
2024-11-16 14:59:30 +0100

    C

--- O-323C-ThreeInARowGame/Program.cs
+++ O-323C-ThreeInARowGame/Program.cs
@@ -9,12 +9,13 @@ class Program
         while (true)
         {
             GameConsole.Show(board);
-            if (!board.IsGameFinished()) break;
             board.IsGameFinished();
             Console.Write("Skriv inn hvor du vil sette kryss (f.eks. \"A2\"): ");
             string position = Console.ReadLine();
             board.Mark(position);
-            Thread.Sleep(1000);
+            GameConsole.Show(board);
+            if (!board.IsGameFinished()) break;
+            Thread.Sleep(2000);
             board.MarkRandom(false);
         }
         Console.WriteLine("Takk for kampen!");
