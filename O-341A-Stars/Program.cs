f3f1f8fd2c7de1b7161665dd75d72d661f0b7dd9
IverHK <iverhk@gmail.com>
2024-11-25 11:53:22 +0100

    d

---
+++ O-341A-Stars/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace O_341A_Stars;
+
+class Program
+{      
+    static void Main(string[] args)
+    {
+        var random = new Random();
+        var stars = new object[]
+        {
+            new PhasesStar(random),
+            new PhasesStar(random),
+            new PhasesStar(random),
+            new MovableStar(random),
+            new MovableStar(random),
+            new MovableStar(random),
+        };
+        while (true)
+        {
+            Console.Clear();     
+            foreach (var star in stars)
+            {
+                if (star is PhasesStar)
+                {
+                    var phasesStar = (PhasesStar)star;
+                    phasesStar.Show();
+                    phasesStar.Update();
+                }
+                else if (star is MovableStar)
+                {
+                    var phasesStar = (MovableStar)star;
+                    phasesStar.Show();
+                    phasesStar.Update();
+                }
+            }
+            Console.CursorLeft = 0;
+            Console.CursorTop = 0;
+            Thread.Sleep(200);
+        }
+    }
+}
