d8e84350256b346f93662bcbdb9f7083e91abc02
IverHK <iverhk@gmail.com>
2024-11-26 17:15:58 +0100

    k

--- O-344-NewsletterSubscription/Program.cs
+++ O-344-NewsletterSubscription/Program.cs
@@ -4,44 +4,45 @@ using O_344_NewsletterSubscription.Model;
 
 namespace O_344_NewsletterSubscription;
 
-
-var emailService = new DummyEmailService();
-var subscriptionRepo = new SubscriptionFileRepository();
-var subscriptionService = new SubscriptionService(emailService, subscriptionRepo);
-
-while (true)
+class Program
 {
-    Console.Write("Meny:\n1: Melde på nyhetsbrev\n2: Verifisere\n");
-    var choice = Console.ReadLine();
-    if (choice == "1")
+    static void Main(string[] args)
     {
-        Console.Write("Skriv inn epostadresse: ");
-        var emailAddress = Console.ReadLine();
-        subscriptionService.Subscribe(emailAddress);
-    }
-    else if (choice == "2")
-    {
-        Console.Write("Skriv inn epostadresse: ");
-        var emailAddress = Console.ReadLine();
-        Console.Write("Skriv inn verifikasjonskode: ");
-        var code = Console.ReadLine();
-        subscriptionService.Verify(emailAddress, code);
+        var emailService = new DummyEmailService();
+        var subscriptionRepo = new SubscriptionFileRepository();
+        var subscriptionService = new SubscriptionService(emailService, subscriptionRepo);
+        // var email = new Email(
+        //     "per@getacademy.no",
+        //     "pål@getacademy.no",
+        //     "Hei",
+        //     "Tekst...");
+        //
+        // emailService.Send(email);
+        //
+        // var subscription = new Subscription("Terje", "terje@getacademy.no");
+        // subscriptionRepo.Save(subscription);
+        //
+        // subscription = subscriptionRepo.Load("terje@getacademy.no");
+        // Console.WriteLine(subscription);
+        
+        while (true)
+        {
+            Console.Write("Meny:\n1: Melde på nyhetsbrev\n2: Verifisere\n");
+            var choice = Console.ReadLine();
+            if (choice == "1")
+            {
+                Console.Write("Skriv inn epostadresse: ");
+                var emailAddress = Console.ReadLine();
+                subscriptionService.Subscribe(emailAddress);
+            }
+            else if (choice == "2")
+            {
+                Console.Write("Skriv inn epostadresse: ");
+                var emailAddress = Console.ReadLine();
+                Console.Write("Skriv inn verifikasjonskode: ");
+                var code = Console.ReadLine();
+                subscriptionService.Verify(emailAddress, code);
+            }
+        }
     }
 }
-
-
-// Demo :
-
-var email = new Email(
-    "per@getacademy.no",
-    "pål@getacademy.no",
-    "Hei",
-    "Tekst...");
-
-emailService.Send(email);
-
-var subscription = new Subscription("Terje", "terje@getacademy.no");
-subscriptionRepo.Save(subscription);
-
-subscription = subscriptionRepo.Load("terje@getacademy.no");
-Console.WriteLine(subscription);
--- O-344-NewsletterSubscription/SubscriptionService.cs
+++ O-344-NewsletterSubscription/SubscriptionService.cs
@@ -1,4 +1,6 @@
+using O_344_NewsletterSubscription.Implementations;
 using O_344_NewsletterSubscription.Interfaces;
+using O_344_NewsletterSubscription.Model;
 
 namespace O_344_NewsletterSubscription
 {
@@ -17,12 +19,51 @@ namespace O_344_NewsletterSubscription
 
         public void Subscribe(string emailAddress)
         {
+            var newUser = new Subscription("New User", emailAddress);
+            var subscriptionRepo = new SubscriptionFileRepository();
+            var emailService = new DummyEmailService();
+            var email = new Email(newUser.Email, "info@getacademy.no", "Vertifikasjonskode", $"Vertifikasjonskoden er: {newUser.VerificationCode}");
 
+            Subscription existingUser = null;
+            
+            
+            try
+            {
+                existingUser = subscriptionRepo.Load(emailAddress);
+                if (existingUser != null && existingUser.Email == newUser.Email)
+                {
+                    Console.WriteLine($"Email {newUser.Email} is already subscribed.");
+                    return;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error loading user: {ex.Message}");
+            }
+            
+            subscriptionRepo.Save(newUser);
+            emailService.Send(email);
+            Console.WriteLine(newUser);
         }
 
         public void Verify(string emailAddress, string verificationCode)
         {
+            var newUser = new Subscription("New User", emailAddress);
+            var subscriptionRepo = new SubscriptionFileRepository();
+            var emailService = new DummyEmailService();
+            var email = new Email(newUser.Email, "info@getacademy.no", "Subscribed!", "Abonnementet på nyhetsbrev er startet");
 
+            newUser = subscriptionRepo.Load(emailAddress);
+            
+            
+            if (verificationCode == newUser.VerificationCode)
+            {
+                    newUser.IsVerified = true;
+                    subscriptionRepo.Save(newUser);
+                    emailService.Send(email);
+                    Console.WriteLine("Exiting, here is new userInfo "+newUser);
+                    Environment.Exit(0);
+            }
         }
     }
 }
